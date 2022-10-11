df419643ae9d95a4e8a29b7061df03c33910ecd1
Daniel <thecow474@gmail.com>
2022-10-11 10:36:47 -0700

    Me when the

---
+++ Main.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+/**
+ * This is to test new knowledge learned in AP Comp Sci
+ * @author Daniel
+ * @version v1
+ */
+public class Main {
+    public static void main(String[] args){
+        out.println("Hello World!");
+        integerTest.main();
+    }
+}
---
+++ integerTest.java
@@ -0,0 +1,6 @@
+public class integerTest {
+    public static void main() {
+        int c = 6;
+        System.out.print(Integer.bitCount(c));
+    }
+}
