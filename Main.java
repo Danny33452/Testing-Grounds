4a5594c5099791924b3563f405b04e26eeb83ecb
Daniel <thecow474@gmail.com>
2022-10-11 13:50:22 -0700

    Me when the

--- Main.java
+++ Main.java
@@ -7,6 +7,5 @@ import static java.lang.System.out;
 public class Main {
     public static void main(String[] args){
         out.println("Hello World!");
-        integerTest.main();
     }
 }
--- integerTest.java
+++ integerTest.java
@@ -1,5 +1,5 @@
 public class integerTest {
-    public static void main() {
+    public static void main(String[] args) {
         int c = 6;
         System.out.print(Integer.bitCount(c));
     }
