47f7f22f82b6a39d011430baf13749d4bc390048
Daniel <thecow474@gmail.com>
2022-10-14 11:01:04 -0700

    Me when the

--- OnedotFiveQuiz.java
+++ OnedotFiveQuiz.java
@@ -1,6 +1,10 @@
 public class OnedotFiveQuiz {
     public static void  main(String[] args){
-
+        int w = 1;
+        int x = w / 2;
+        double y = 3;
+        int z = (int) (x + y);
+        System.out.print(z);
     }
 }
 
