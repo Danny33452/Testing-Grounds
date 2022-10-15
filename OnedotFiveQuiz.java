beb4d04019c98c0a6291016ce81d2401ddaecaad
Daniel <thecow474@gmail.com>
2022-10-14 21:38:31 -0700

    Me when the

--- OnedotFiveQuiz.java
+++ OnedotFiveQuiz.java
@@ -1,10 +1,34 @@
 public class OnedotFiveQuiz {
     public static void  main(String[] args){
+        /*
         int w = 1;
         int x = w / 2;
         double y = 3;
         int z = (int) (x + y);
         System.out.print(z);
+#
+        double x = 4.5;
+        int y = (int) x * 2;
+        System.out.print(y);
+#
+        int x = 4;
+        int y = 5;
+        int sum = x + y;
+        double average = sum / 2;
+        System.out.println(average);
+        double num = 9 / 4;
+
+        System.out.print(num);
+
+        System.out.print(" ");
+
+        System.out.print((int) num);
+        */
+        double x = (int) (5.5 - 2.5);
+        System.out.println(x);
+        double y = (int) 5.5 - 2.5;
+        System.out.println(y);
+        System.out.println(x - y);
     }
 }
 
