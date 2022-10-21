f9d1a65bb1d7273e72b552cc8385713fcc84eae4
Daniel <thecow474@gmail.com>
2022-10-21 09:50:16 -0700

    Me when the

--- Song.java
+++ Song.java
@@ -14,6 +14,6 @@ public class Song {
         }
     }
     public static void line(int lineNumber) {
-        System.out.println(lineNumber + " bottles of beer on the wall " + lineNumber + " bottles of beer\ntake one down pass it around " + (lineNumber - 1) + " bottles of beer");
+        System.out.println(lineNumber + " bottles of beer on the wall " + lineNumber + " bottles of beer\ntake one down pass it around " + (lineNumber - 1) + " bottles of beer on the wall.");
     }
 }
