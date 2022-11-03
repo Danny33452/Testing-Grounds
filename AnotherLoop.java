5f0f2424d015934ae6dc7caaebc3ccd354646254
Daniel <thecow474@gmail.com>
2022-11-02 17:50:14 -0700

    Me when the

--- AnotherLoop.java
+++ AnotherLoop.java
@@ -2,19 +2,19 @@ import static java.lang.System.*;
 
 public class AnotherLoop {
     public static void main(String[] args) {
-      int lineAmount=5;
+      int lineAmount=30;
       pyrimid(lineAmount);
     }
     public static void pyrimid(int lineAmount){
         for(int i = 1; i<=lineAmount; i++){
-            if (4 - (2 * (int)i) >=0){
-                out.print(" ".repeat(4-(2*((int)i))));
+            if (i != 0) {
+                out.print(" ".repeat(2*lineAmount-2*i));
             }
-            out.print("*".repeat((int)i));
+            out.print("*".repeat(i));
             out.print("//");
-            out.print("X".repeat((int)i*2));
+            out.print("X".repeat(i*2));
             out.print("\\\\");
-            out.println("*".repeat((int)i));
+            out.println("*".repeat(i));
         }
     }
 }
